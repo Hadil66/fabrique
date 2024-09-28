72887b0d89922c750b30eb2ef443550e2d6571d8
Hadil24A <hadil.ahmed@hva.nl>
2024-09-28 15:52:09 +0200

    #22
    Dropdownmenu HTML

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<nav>
+    <button id="open-menu">
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none">
+            <g id="Menu / Menu_Duo_MD">
+            <path id="Vector" d="M5 15H19M5 9H19" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </g>
+            </svg>
+    </button>
+    <div id="dropdownmenu">
+        <div id="nav-buttons">
+            <button> tickets </button>
+            <button><svg viewBox="-10 -10 50 50" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"><path d="M13 29.3c-3.1-3.8-9.9-12.9-9.9-18C3.1 5 8.1 0 14.4 0c6.2 0 11.3 5 11.3 11.3 0 5.1-6.9 14.3-9.9 18-.8.9-2.1.9-2.8 0zM14.4 15a3.8 3.8 0 1 0 0-7.6 3.8 3.8 0 0 0 0 7.6z"></path></svg> venues </button>
+            <button> العربية </button>
+            <button> <svg fill="currentColor" height="30" viewBox="0 0 30 30" width="30" xmlns="http://www.w3.org/2000/svg"><path d="m20.7 10.8-1.5-1.5-4.2 4.3-4.2-4.3-1.5 1.5 4.3 4.2-4.3 4.2 1.5 1.5 4.2-4.3 4.2 4.3 1.5-1.5-4.3-4.2z"></path></svg> close </button>
+        </div>
+
+            <ul id="nav-1">
+                <a href="#">قصصنا stories </a>
+                <a href="#"> الفعاليات what's on</a>
+                <a href="#"> زيارة visit </a>
+            </ul>
+
+            <h3>Find everything for </h3>
+            <ul id="nav-2">
+                <li> families and children </li>
+                <li> schools </li>
+                <li> researchers </li>
+                <li> shop </li>
+                <li> about us </li>
+            </ul>
+
+            <ul class="nav-3">
+                <li> careers </li>
+                <li> press </li>
+                <li> corporate </li>
+                <li> host your event </li>
+                <li> contact </li>
+                <li> accessability </li>
+                <li> terms and conditions </li>
+                <li> cookie policy </li>
+            </ul>
+
+            <svg class="logo__svg logo__svg--en" height="150" preserveAspectRatio="xMinYMid meet" viewBox="0 0 627 150" width="627" x="0" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" y="0"><use fill="currentColor" x="0" xlink:href="#logo-en" y="0"></use></svg>
+        </div>
+</nav>
