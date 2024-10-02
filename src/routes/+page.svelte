fd84756cf57888ed1494ddecf689af1c6c0084c2
RukiyaTossou <114156045+RukiyaTossou@users.noreply.github.com>
2024-10-02 13:04:10 +0200

    nav bar without content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,23 @@
 <nav>
     <div class="logo"></div>
+    <h1>Qatar Museums</h1>
     <ul>
-        <li>Stories</li>
-        <li>What's on</li>
-        <li>Collection</li>
-        <li>Visit</li>
+        <li>
+            <span> قصص</span>  <!-- Arabic for Stories -->
+            Stories
+        </li>
+        <li>
+            <span> ما هو الجديد</span>  <!-- Arabic for What's on -->
+            What's on
+        </li>
+        <li>
+            <span> المجموعة</span>  <!-- Arabic for Collection -->
+            Collection
+        </li>
+        <li>
+            <span> زيارة </span>  <!-- Arabic for Visit -->
+            Visit
+        </li>
         <li>tickets</li>
         <li>members</li>
         <li>venues</li>
