f82e309080cb87fc3cfbc52005ccf14a9a733c0e
Hadil24A <hadil.ahmed@hva.nl>
2024-10-03 16:34:48 +0200

    Squashed commit of the following:
    commit 83462525714d87d976f4ba17bb63a685f0773327
    Merge: ebb7744 801dfef
    Author: Nadira Ben Salah <144009698+Naddybs@users.noreply.github.com>
    Date:   Thu Oct 3 16:31:52 2024 +0200
    
        Merge pull request #1 from Hadil66/nadira-datafetch
    
        Data ophalen & weergeven van Directus Api
    
    commit 801dfefa436e5a6b587f331f836807c5fa8d8a2c
    Author: Naddybs <144009698+Naddybs@users.noreply.github.com>
    Date:   Wed Oct 2 00:27:51 2024 +0200
    
        Data ophalen & weergeven
    
    commit ebb7744d982f302558d0c7c5277363bebb241a7b
    Author: RukiyaTossou <114156045+RukiyaTossou@users.noreply.github.com>
    Date:   Sat Sep 28 13:24:18 2024 +0200
    
        global css file created

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let data;
+    console.log(data); // Hiermee kun je zien hoe de API-respons eruitziet
+</script>
+
+{#each data.artObjects as art}
+    <ul>
+        <li>
+            <h2>{art.title}</h2>
+            <img src={"https://fdnd-agency.directus.app/assets/" + art.image} alt={art.title} />
+        </li>
+    </ul>
+{/each}
+
