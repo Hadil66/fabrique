7827b141bf58a619e4bdb6cbba440b15cf7b4a11
EBok1 <144004887+EBok1@users.noreply.github.com>
2024-10-06 17:00:51 +0200

    standaard tekst verwijderd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,15 +20,6 @@
     <div class="salamander swims walks">Salamander</div>
   </div>
 
-  <div class="wrap">
-    <div class="search">
-       <input type="text" class="searchTerm" placeholder="What are you looking for?">
-       <button type="submit" class="searchButton">
-         <i class="fa fa-search"></i>
-      </button>
-    </div>
- </div>
-
 <style>
 button[data-filter="walks"]:focus ~ div:not([class*="walks"]) {
   display:none;
