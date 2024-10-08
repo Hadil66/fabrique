9372ad2ae99d3b260549690ea9775f046da26e24
EBok1 <144004887+EBok1@users.noreply.github.com>
2024-10-08 15:41:18 +0200

    Ellenoor filters (#8)
    * filter
    
    oefenen met maken van een filter
    
    * Create fetch-json bestand
    
    * standaard tekst verwijderd
    
    * Create +page.server.js
    
    * overbodige code verwijderd
    
    * prettier toegepast
    
    * tab focus functie aangepast
    
    * styling filter
    
    filter is placed in the middle of the page
    
    ---------
    
    Co-authored-by: Hadil <144008714+Hadil66@users.noreply.github.com>

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -12,3 +12,4 @@ export async function load(){
         artObjects: response.data
     };
 }
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,6 +24,15 @@ export let data;
     {/each}
     </ul>
 
+<!-- Filters workspace -->
+<div class="filteredList">
+	<button class="filter-option" data-filter="*" tabindex="0">All objects</button>
+	<button class="filter-option" data-filter="Pottery" tabindex="0">Pottery</button>
+	<button class="filter-option" data-filter="Islamic art" tabindex="0">Islamic art</button>
+	<button class="filter-option" data-filter="Tapestry" tabindex="0">Tapestry</button>
+	<button class="filter-option" data-filter="Glass" tabindex="0">Glass</button>
+  </div>
+
 <div class="wrap">
 	<div class="search">
 		<input
@@ -36,7 +45,6 @@ export let data;
 			<Search />
 		</button>
 	</div>
-</div>
 
 <style>
 	* {
@@ -130,6 +138,39 @@ export let data;
             font-size: 1.5rem;
             }
       }
+ 
+  	.filter-option {
+		background-color: #464646;
+		color: white;
+		border: none;
+		height: 50px;
+		width: 100px;
+		font-size: 19px;
+		margin-right: 0.5em;
+	}
+
+	.filter-option:focus {
+		outline: 2px solid #007bff;
+		box-shadow: 0 0 0 2px #007bff;
+		background-color: #007bff;
+		color: #ffffff;
+	}
+
+	.filteredList {
+		position: absolute;
+		bottom: 5em;
+		overflow: scroll;
+		display: flex;
+	}
+
+	@media only screen and (min-width: 600px) {
+		.filteredList {
+			position: absolute;
+			left: 50%;
+			transform: translateX(-50%);
+			bottom: 6.7em;
+		}
+	}
 
 	.search {
 		width: 100vw;
