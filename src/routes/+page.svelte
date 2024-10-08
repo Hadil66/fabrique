9c614c754ce6fa2b02a53b655286502c61fd9b71
EBok1 <144004887+EBok1@users.noreply.github.com>
2024-10-08 15:55:27 +0200

    Zoekbalk werkend

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,39 @@
 <script>
-export let data;
-    console.log(data); // Hiermee kun je zien hoe de API-respons eruitziet
+	export let data;
+	console.log(data); // Hiermee kun je zien hoe de API-respons eruitziet
 	import Search from '$lib/Search.svelte'; // Icoon wordt gebruikt voor de searchbar
 	let filterText = '';
-  import Navbar from '$lib/navbar.svelte';
-
+	import Navbar from '$lib/navbar.svelte';
 </script>
 
 <Navbar />
 
 <ul class="masonry">
-    {#each data.artObjects as art}
-        <li class="masonry-item">
-            <figure>
-                <img src={"https://fdnd-agency.directus.app/assets/" + art.image} alt={art.title} />
-                <figcaption>
-                    <h2>{art.title}</h2>
-                    <a href="#" class="button">Meer info</a>
-               </figcaption>
-            </figure>
-
-        </li>
-    {/each}
-    </ul>
+	{#each data.artObjects.filter((art) => art.title
+			.toLowerCase()
+			.includes(filterText.toLowerCase())) as art}
+		<li class="masonry-item">
+			<figure>
+				<img src={'https://fdnd-agency.directus.app/assets/' + art.image} alt={art.title} />
+				<figcaption>
+					<h2>{art.title}</h2>
+					<a href="#" class="button">Meer info</a>
+				</figcaption>
+			</figure>
+		</li>
+	{/each}
+</ul>
 
 <!-- Filters workspace -->
+<input type="text" bind:value={filterText} placeholder="Search by title" />
+
 <div class="filteredList">
 	<button class="filter-option" data-filter="*" tabindex="0">All objects</button>
 	<button class="filter-option" data-filter="Pottery" tabindex="0">Pottery</button>
 	<button class="filter-option" data-filter="Islamic art" tabindex="0">Islamic art</button>
 	<button class="filter-option" data-filter="Tapestry" tabindex="0">Tapestry</button>
 	<button class="filter-option" data-filter="Glass" tabindex="0">Glass</button>
-  </div>
+</div>
 
 <div class="wrap">
 	<div class="search">
@@ -45,102 +47,106 @@ export let data;
 			<Search />
 		</button>
 	</div>
-    </div>
+</div>
 
 <style>
 	* {
 		transition: 0.2s;
 		font-family: 'DIN Next', sans-serif;
 	}
-  
-          .masonry {
-            column-count: 1;
-            column-gap: 1rem;
-            list-style: none;
-            padding: 0;
-        }
-    
-        .masonry-item {
-            break-inside: avoid;
-            margin-bottom: 1rem;
-            position: relative;
-            overflow: hidden;
-            background-color: #fff;
-            border-radius: 8px;
-            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-        }
 
-        figure{
-            margin: 0;
-            position: relative;
-        }
-    
-        img {
-            width: 100%;
-            height: auto;
-            display: block;
-            border-radius: 8px;
-            transition: transform 0.3s ease-in-out;
-        }
+	.masonry {
+		column-count: 1;
+		column-gap: 1rem;
+		list-style: none;
+		padding: 0;
+	}
 
-        /* overlay */
+	.masonry-item {
+		break-inside: avoid;
+		margin-bottom: 1rem;
+		position: relative;
+		overflow: hidden;
+		background-color: #fff;
+		border-radius: 8px;
+		box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+	}
 
-        figcaption{
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            background-color: rgba(0, 0, 0, 0.6);
-            color: white;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            opacity: 0;
-            transition: opacity 0.3s ease-in-out;
-        }
+	figure {
+		margin: 0;
+		position: relative;
+	}
 
-        .masonry-item:hover img{
-            transform: scale(1.1);
-        }
-        .masonry-item:hover figcaption{
-            opacity: 1;
-        }
+	img {
+		width: 100%;
+		height: auto;
+		display: block;
+		border-radius: 8px;
+		transition: transform 0.3s ease-in-out;
+	}
 
-        h2 {
-            font-size: 16px;
-            margin: 0.5rem 0;
-        }
+	/* overlay */
 
-        a{
-            color: black;
-            background-color: #efc715;
-            padding: 0.5rem 1rem;
-            border-radius: 4px;
-            text-decoration: none;
-            &:hover{
-                background-color: #00b0f0;
-            }
-        }
-    
-        /* Grotere schermen: 2 kolommen voor medium en 3 voor grote schermen */
-        @media (min-width: 600px) {
-            .masonry {
-                column-count: 2;
-            }
-        }
-    
-        @media (min-width: 900px) {
-            .masonry {
-                column-count: 3;
-            }
-            h2{
-            font-size: 1.5rem;
-            }
-      }
- 
-  	.filter-option {
+	figcaption {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(0, 0, 0, 0.6);
+		color: white;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		opacity: 0;
+		transition: opacity 0.3s ease-in-out;
+	}
+
+	.masonry-item:hover img {
+		transform: scale(1.1);
+	}
+	.masonry-item:hover figcaption {
+		opacity: 1;
+	}
+
+	h2 {
+		font-size: 16px;
+		margin: 0.5rem 0;
+	}
+
+	a {
+		color: black;
+		background-color: #efc715;
+		padding: 0.5rem 1rem;
+		border-radius: 4px;
+		text-decoration: none;
+		&:hover {
+			background-color: #00b0f0;
+		}
+	}
+
+	/* Grotere schermen: 2 kolommen voor medium en 3 voor grote schermen */
+	@media (min-width: 600px) {
+		.masonry {
+			column-count: 2;
+		}
+	}
+
+	@media (min-width: 900px) {
+		.masonry {
+			column-count: 3;
+		}
+		h2 {
+			font-size: 1.5rem;
+		}
+	}
+
+	/* ---- */
+	/* Styling Ellenoor */
+	/* ---- */
+	/* Styling voor filter buttons */
+	.filter-option {
 		background-color: #464646;
 		color: white;
 		border: none;
@@ -150,6 +156,7 @@ export let data;
 		margin-right: 0.5em;
 	}
 
+	/* Styling voor tab functie */
 	.filter-option:focus {
 		outline: 2px solid #007bff;
 		box-shadow: 0 0 0 2px #007bff;
@@ -164,15 +171,7 @@ export let data;
 		display: flex;
 	}
 
-	@media only screen and (min-width: 600px) {
-		.filteredList {
-			position: absolute;
-			left: 50%;
-			transform: translateX(-50%);
-			bottom: 6.7em;
-		}
-	}
-
+	/* Styling voor de zoekbalk */
 	.search {
 		width: 100vw;
 		position: relative;
@@ -205,13 +204,21 @@ export let data;
 		cursor: pointer;
 	}
 
-	/*Resize the wrap to see the search bar change!*/
 	.wrap {
 		position: absolute;
 		bottom: 0;
 	}
 
 	@media only screen and (min-width: 600px) {
+		/* Code voor filter buttons */
+		.filteredList {
+			position: absolute;
+			left: 50%;
+			transform: translateX(-50%);
+			bottom: 6.7em;
+		}
+
+		/* Code voor zoekbalk  */
 		.wrap {
 			position: absolute;
 			bottom: 4em;
