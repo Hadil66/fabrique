52ce0fb5df7cd4913992d479fb404fb7a9b19ca0
EBok1 <144004887+EBok1@users.noreply.github.com>
2025-01-13 14:58:22 +0100

    #53
    artObject in component
    navbar delete
    error page delete

---
+++ src/lib/molecules/ArtObject.svelte
@@ -0,0 +1,121 @@
+<script>
+	export let art
+</script>
+
+<li class="masonry-item">
+	<figure>
+		<picture>
+			<source
+				srcset={"https://fdnd-agency.directus.app/assets/" +
+				art.image +
+				".avif"}
+				type="image/avif"
+			/>
+			<source
+				srcset={"https://fdnd-agency.directus.app/assets/" +
+				art.image +
+				".webp"}
+				type="image/webp"
+			/>
+			<img
+				src={"https://fdnd-agency.directus.app/assets/" + art.image}
+				alt={art.title}
+				height={art.height}
+				width={art.width}
+				loading="lazy"
+			/>
+		</picture>
+
+		<figcaption>
+			<h2>{art.title}</h2>
+		</figcaption>
+	</figure>
+</li>
+
+<style>
+    .masonry-item {
+		break-inside: avoid;
+		display: block;
+		background-color: #fff;
+		border-radius: 8px;
+		box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+		margin-bottom: 1rem;
+		position: relative;
+		overflow: hidden;
+		transition: opacity 0.3s;
+	}
+
+	.masonry-item.hidden {
+		filter: opacity(0.3);
+		pointer-events: none;
+		transition: 1s;
+	}
+
+	.masonry-item:focus {
+		outline: 2px solid #020202;
+		outline-offset: 3px;
+	}
+
+	.masonry-item:hover img,
+	.masonry-item:focus img {
+		transform: scale(1.1);
+	}
+
+	.masonry-item:hover figcaption,
+	.masonry-item:focus figcaption {
+		opacity: 1;
+	}
+
+	figure {
+		margin: 0;
+		position: relative;
+	}
+
+	img {
+		width: 100%;
+		height: auto;
+		display: block;
+		border-radius: 8px;
+		transition: transform 0.3s ease-in-out;
+	}
+
+	figcaption {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(0, 0, 0, 0.6);
+		color: white;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		opacity: 0;
+		transition: opacity 0.3s ease-in-out;
+	}
+
+	h2 {
+		font-size: 16px;
+		margin: 0.5rem 0;
+		text-align: center;
+		-webkit-text-stroke: 0.2px #ffff00;
+	}
+
+    @media (min-width: 900px) {
+		h2 {
+			font-size: 1.5rem;
+		}
+	}
+
+	/* Prefers reduced motion */
+	@media (prefers-reduced-motion) {
+		.masonry-item {
+			transition: none;
+		}
+
+		img {
+			transition: none;
+		}
+	}
+</style>
