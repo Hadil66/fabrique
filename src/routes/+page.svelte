f9488e27696bec3238b664b9fe74ba642e13837e
EBok1 <144004887+EBok1@users.noreply.github.com>
2025-01-14 23:05:59 +0100

    Ellenoor vertical scroll (#12)
    * Install lenis package
    
    * import lenis
    
    * tabs
    
    ---------
    
    Co-authored-by: Hadil <144008714+Hadil66@users.noreply.github.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,16 @@
 <script>
 	import { activeFilter } from "$lib/store";
 	import Header from '$lib/molecules/Header.svelte';
+  import ArtObject from '$lib/molecules/ArtObject.svelte';
+  import Lenis from '@studio-freight/lenis';
+  import { onMount, onDestroy } from 'svelte';
 	import Searchbar from '$lib/molecules/searchbar.svelte';
-	import ArtObject from '$lib/molecules/ArtObject.svelte';
 	import Filters from "$lib/molecules/Filters.svelte";
 
 	export let data;
-
+  
+  
+  let lenis;
 	let filterText = '';
 	let scrollContainer; 
 
@@ -19,11 +23,33 @@
 	function pastHalfScrollWidth() {
 		return scrollContainer.scrollLeft >= scrollContainer.scrollWidth / 2
 	}
-
-	function backToHalfScrollWidth(){
+	
+function backToHalfScrollWidth(){
 		scrollContainer.scrollLeft -= scrollContainer.scrollWidth / 2;
 	}
-</script>
+
+  			onMount(() => {
+			// Initialize Lenis
+			lenis = new Lenis({ infinite: true, syncTouch: true });
+
+			// Define the requestAnimationFrame function
+			function onRaf(time) {
+			lenis.raf(time);
+			requestAnimationFrame(onRaf);
+			}
+
+			// Start the animation frame loop
+			requestAnimationFrame(onRaf);
+			});
+
+			onDestroy(() => {
+				// Cleanup if necessary (e.g., stop the animation frame)
+				lenis = null; // or any other cleanup logic if required
+			});console.log('Scroll container:', scrollContainer);
+			console.log('Active filter:', $activeFilter);
+			console.log('Techniques:', techniques);
+			console.log('Lenis:', lenis);
+  </script>
 
 <Header />
 
