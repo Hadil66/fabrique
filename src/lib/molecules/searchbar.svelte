2c740c9afbfe432a5d6a2668dc6ed94709d31c66
rukiyaT <jteesje@gmail.com>
2025-01-23 20:33:47 +0100

    enter gaat ook naar andere pagina

--- src/lib/molecules/searchbar.svelte
+++ src/lib/molecules/searchbar.svelte
@@ -1,116 +1,142 @@
 <script>
-    	import SearchIcon from '$lib/SearchIcon.svelte'; 
+	import SearchIcon from '$lib/SearchIcon.svelte';
+    import { goto } from '$app/navigation';
 
-        let filterText = '';
+
+    function handleKeydown(event) {
+    if (event.key === 'Enter') {
+      event.preventDefault(); // Voorkom het standaard gedrag
+      submitSearch();
+    }
+  }
+
+  function submitSearch() {
+    const baseUrl = 'https://qm.org.qa/en/'; // De basis URL, zodat deze makkelijk akn worden aangepast 
+    const searchTerm = encodeURIComponent(filterText); // De zoekterm
+    const searchUrl = `${baseUrl}?query=${searchTerm}`; // Voeg de zoekterm toe aan de URL
+
+    // Navigeer naar de zoekpagina
+    window.location.href = searchUrl;
+  }
+	let filterText = '';
+
 </script>
 
 <div class="wrap">
+
+
 	<div class="search">
 		<input
 			type="text"
 			class="searchTerm"
 			bind:value={filterText}
 			placeholder="Search the collection"
+            on:keydown={handleKeydown}
 		/>
-        <form action="https://qm.org.qa/en/">
-		<button type="submit"  action="https://qm.org.qa/en/" class="searchButton" aria-label="Submit search">
-			<SearchIcon />
-		</button>
-    </form>
+		<form action="https://qm.org.qa/en/">
+			<button
+				type="submit"
+				action="https://qm.org.qa/en/"
+				class="searchButton"
+				aria-label="Submit search"
+			>
+				<SearchIcon />
+			</button>
+		</form>
 	</div>
 </div>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@500&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@500&display=swap');
 
-.search {
-    width: 100vw;
-    position: relative;
-    display: flex;
-    align-items: center; 
-    border: 2px solid rgb(14, 14, 14); 
-    border-radius: 4px; 
-    overflow: hidden;
-    background: white;
-}
+	.search {
+		width: 100vw;
+		position: relative;
+		display: flex;
+		align-items: center;
+		border: 2px solid rgb(14, 14, 14);
+		border-radius: 4px;
+		overflow: hidden;
+		background: white;
+	}
 
-.search::after {
-    content: '';
-    position: absolute;	
-    bottom: 0.5em;
-    left: 0.5rem;
-    width: 93%;
-    height: 1px; 
-    background-color: black;
-}
+	.search::after {
+		content: '';
+		position: absolute;
+		bottom: 0.5em;
+		left: 0.5rem;
+		width: 93%;
+		height: 1px;
+		background-color: black;
+	}
 
-.searchTerm {
-    flex: 1; 
-    border: none; 
-    padding: 0.5em 0.5em;
-    font-family: 'Roboto condensed', sans-serif;
-    font-size: 24px;
-    background: transparent;
-}
+	.searchTerm {
+		flex: 1;
+		border: none;
+		padding: 0.5em 0.5em;
+		font-family: 'Roboto condensed', sans-serif;
+		font-size: 24px;
+		background: transparent;
+	}
 
-/* text styling */
-.searchTerm::placeholder {
-    color: #000000;
-    text-transform: uppercase;
-    font-weight: 500; 
-    letter-spacing: 0.1rem;
-}
+	/* text styling */
+	.searchTerm::placeholder {
+		color: #000000;
+		text-transform: uppercase;
+		font-weight: 500;
+		letter-spacing: 0.1rem;
+	}
 
-.searchButton {
-    width: 4em;
-    height: 100%; 
-    border: none; 
-    background: transparent; 
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding: 0;
-    margin-left: 0.3rem;
+	.searchButton {
+		width: 4em;
+		height: 100%;
+		border: none;
+		background: transparent;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		padding: 0;
+		margin-left: 0.3rem;
 
-    cursor: pointer;
-}
+		cursor: pointer;
+	}
 
-.searchButton:hover {
-    transform: scale(1.2);
-}
+	.searchButton:hover {
+		transform: scale(1.2);
+	}
 
-.wrap {
-    position: fixed;
-    bottom: 0;
-    width: 100%;
-}
+	.wrap {
+		position: fixed;
+		bottom: 0;
+		width: 100%;
+	}
 
-@keyframes loadingAnimation {
-0% {
-    width: 0;
-}
-50% {
-    width: 100%;
-}
-100% {
-    width: 0; 
-}
-}
+	@keyframes loadingAnimation {
+		0% {
+			width: 0;
+		}
+		50% {
+			width: 100%;
+		}
+		100% {
+			width: 0;
+		}
+	}
 
-@media only screen and (min-width: 600px) {
-    .wrap {
-        position: absolute;
-        bottom: 6em;
-        left: 2em;
-        right: 2em;
-    }
+	@media only screen and (min-width: 600px) {
+		.wrap {
+			position: absolute;
+			bottom: 6em;
+			left: 2em;
+			right: 2em;
+		}
 
-    .search {
-        width: 60vw;
-        position: fixed;
-        display: flex;
-        left: 50%;
-        transform: translateX(-50%);
-    }
-}
+		.search {
+			width: 60vw;
+			position: fixed;
+			display: flex;
+			left: 50%;
+			transform: translateX(-50%);
+		}
+	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,106 +1,110 @@
 <script>
-  import { activeFilter } from "$lib/store";
-  import Header from "$lib/molecules/Header.svelte";
-  import ArtObject from "$lib/molecules/ArtObject.svelte";
-  import Lenis from "@studio-freight/lenis";
-  import { onMount, onDestroy } from "svelte";
-  import Searchbar from "$lib/molecules/searchbar.svelte";
-  import Filters from "$lib/molecules/Filters.svelte";
+	import { activeFilter } from '$lib/store';
+	import Header from '$lib/molecules/Header.svelte';
+	import ArtObject from '$lib/molecules/ArtObject.svelte';
+	import Lenis from '@studio-freight/lenis';
+	import { onMount, onDestroy } from 'svelte';
+	import Searchbar from '$lib/molecules/searchbar.svelte';
+	import Filters from '$lib/molecules/Filters.svelte';
 
-  export let data;
+	export let data;
 
-  let lenis;
-  let scrollContainer;
+	export let filterText;
+	let lenis;
+	let scrollContainer;
 
-  function handleScroll() {
-    if (pastHalfScrollWidth()) {
-      backToHalfScrollWidth();
-    }
-  }
 
-  function pastHalfScrollWidth() {
-    return scrollContainer.scrollLeft >= scrollContainer.scrollWidth / 2;
-  }
 
-  function backToHalfScrollWidth() {
-    scrollContainer.scrollLeft -= scrollContainer.scrollWidth / 2;
-  }
 
-  onMount(() => {
-    // Initialize Lenis
-    lenis = new Lenis({ infinite: true, syncTouch: true });
 
-    // Define the requestAnimationFrame function
-    function onRaf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(onRaf);
-    }
+// console.log('art titile', art.title);
 
-    // Start the animation frame loop
-    requestAnimationFrame(onRaf);
-  });
+	function handleScroll() {
+		if (pastHalfScrollWidth()) {
+			backToHalfScrollWidth();
+		}
+	}
 
-  onDestroy(() => {
-    lenis = null;
-  });
-  console.log("Scroll container:", scrollContainer);
+	function pastHalfScrollWidth() {
+		return scrollContainer.scrollLeft >= scrollContainer.scrollWidth / 2;
+	}
 
-  console.log("Lenis:", lenis);
+	function backToHalfScrollWidth() {
+		scrollContainer.scrollLeft -= scrollContainer.scrollWidth / 2;
+	}
 
-  const techniques = ["Pottery", "Islamic art", "Tapestry", "Glass"];
+	onMount(() => {
+		// Initialize Lenis
+		lenis = new Lenis({ infinite: true, syncTouch: true });
+
+		// Define the requestAnimationFrame function
+		function onRaf(time) {
+			lenis.raf(time);
+			requestAnimationFrame(onRaf);
+		}
+
+		// Start the animation frame loop
+		requestAnimationFrame(onRaf);
+	});
+
+	onDestroy(() => {
+		lenis = null;
+	});
+	console.log('Scroll container:', scrollContainer);
+
+	console.log('Lenis:', lenis);
+
+	const techniques = ['Pottery', 'Islamic art', 'Tapestry', 'Glass'];
 </script>
 
 <Header />
 
-<div
-  class="scroll-container"
-  bind:this={scrollContainer}
-  on:scroll={handleScroll}
->
-  <ul class="masonry">
-    {#each data.artObjects as art, index}
-      <ArtObject {art} {techniques} {index} />
-    {/each}
-  </ul>
+<div class="scroll-container" bind:this={scrollContainer} on:scroll={handleScroll}>
+	<ul class="masonry">
+		{#each data.artObjects as art, index}
+			<ArtObject {art} {techniques} {index} />
+		{/each}
+	</ul>
 </div>
 
 <div>
-  <Searchbar />
-  <Filters />
+	<Searchbar bind:filterText={filterText} /> 
+	
+	<Filters />
 </div>
 
 <style>
-  .scroll-container {
-    display: grid;
-    overflow-x: auto;
-    padding: 1rem;
-    margin: 2.5rem;
-    scroll-snap-type: x mandatory;
-  }
+	.scroll-container {
+		display: grid;
+		overflow-x: auto;
+		padding: 1rem;
+		margin: 2.5rem;
+		scroll-snap-type: x mandatory;
+	}
 
-  .masonry {
-    column-count: 1;
-    column-gap: 1rem;
-    list-style: none;
-    padding: 0;
-    width: 200vw;
-  }
+	.masonry {
+		column-count: 1;
+		column-gap: 1rem;
+		list-style: none;
+		padding: 0;
+		width: 200vw;
+	}
 
-  @media (min-width: 600px) {
-    .masonry {
-      column-count: 4;
-    }
-  }
+	@media (min-width: 600px) {
+		.masonry {
+			column-count: 4;
+		}
+	}
 
-  @media (min-width: 700px) {
-    .masonry {
-      column-count: 5;
-    }
-  }
+	@media (min-width: 700px) {
+		.masonry {
+			column-count: 5;
+		}
+	}
 
-  @media (min-width: 900px) {
-    .masonry {
-      column-count: 7;
-    }
-  }
+	@media (min-width: 900px) {
+		.masonry {
+			column-count: 7;
+		}
+	}
 </style>
